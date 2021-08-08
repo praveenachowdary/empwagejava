753d286d2a18590253fbab7b76b31de3f57a206b
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-08 14:19:14 +0530

    cal wage till a cond

--- src/com/praveena/wage/Employeewage.java
+++ src/com/praveena/wage/Employeewage.java
@@ -5,31 +5,30 @@ public class Employeewage {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
     public static void main(String[] args) {
         int empHrs = 0;
-        int salary = 0;
-        int totalsalarymonth = 0;
-        for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS )
         {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch(empCheck)
             {
                 case IS_PART_TIME:
                     empHrs = 4;
-                    System.out.println("parttime");
                     break;
                 case IS_FULL_TIME:
                     empHrs = 8;
-                    System.out.println("fulltime");
                     break;
                 default:
                     empHrs = 0;
-                    System.out.println("absent");
             }
-            salary = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("salary: "+ salary);
-            totalsalarymonth=totalsalarymonth+salary;
+            totalEmpHrs += empHrs;
+            System.out.println("Day#: " + totalWorkingDays + " empHrs: " +empHrs);
         }
-        System.out.println("Total salary for month: " + totalsalarymonth);
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
