e22497ce900eddd19f68e420449a116e8c155da0
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-08 13:55:05 +0530

    check weather employee is present or absent

--- src/com/praveena/wage/Employeewage.java
+++ src/com/praveena/wage/Employeewage.java
@@ -3,5 +3,13 @@ package com.praveena.wage;
 public class Employeewage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage program");
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck ==  IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
