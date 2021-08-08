3c2b1a05deb109a0d340c3d3605603aa9ec346c6
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-08 14:14:03 +0530

    switch usecase statement

--- src/com/praveena/wage/Employeewage.java
+++ src/com/praveena/wage/Employeewage.java
@@ -1,25 +1,26 @@
 package com.praveena.wage;
 
 public class Employeewage {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int empRatePerHr = 20;
     public static void main(String[] args) {
-        System.out.println("welcome to employee wage program");
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int empRatePerHr = 20;
-        int salary;
-        int empHrs;
-        double empcheck = Math.floor(Math.random() * 10) % 3;
-        if (empcheck ==  IS_FULL_TIME) {
-            System.out.println("Employee is fulltime present");
-            empHrs = 8;
-        }
-        else if (empcheck == IS_PART_TIME){
-            System.out.println("Employee is parttime");
-            empHrs = 4;
-        }
-        else {
-            System.out.println("Employee is absent");
-            empHrs = 0;
+        int salary=0;
+        int empHrs=0;
+        int empcheck =(int) Math.floor(Math.random() * 10) % 3;
+        switch(empcheck){
+            case IS_FULL_TIME:
+                System.out.println("Employee is fulltime present");
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee is parttime");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+                empHrs = 0;
+                break;
         }
         salary = empHrs * empRatePerHr;
         System.out.println("salary is " +salary);
