a6c9b4528dbacaf1a4d63332c8fc1ab9c3ee6acf
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-08 14:16:00 +0530

    calculate wage for a month

--- src/com/praveena/wage/Employeewage.java
+++ src/com/praveena/wage/Employeewage.java
@@ -1,28 +1,35 @@
 package com.praveena.wage;
 
 public class Employeewage {
-    public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
-    public static final int empRatePerHr = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
     public static void main(String[] args) {
-        int salary=0;
-        int empHrs=0;
-        int empcheck =(int) Math.floor(Math.random() * 10) % 3;
-        switch(empcheck){
-            case IS_FULL_TIME:
-                System.out.println("Employee is fulltime present");
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is parttime");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
-                empHrs = 0;
-                break;
+        int empHrs = 0;
+        int salary = 0;
+        int totalsalarymonth = 0;
+        for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+        {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch(empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    System.out.println("parttime");
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    System.out.println("fulltime");
+                    break;
+                default:
+                    empHrs = 0;
+                    System.out.println("absent");
+            }
+            salary = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("salary: "+ salary);
+            totalsalarymonth=totalsalarymonth+salary;
         }
-        salary = empHrs * empRatePerHr;
-        System.out.println("salary is " +salary);
+        System.out.println("Total salary for month: " + totalsalarymonth);
     }
 }
